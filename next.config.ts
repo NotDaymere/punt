d6479a4b0c72b0059b43779b72405f9626789223
NotDaymere <ikonotopchick@gmail.com>
2025-02-09 16:12:56 +0200

    disable lint

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
-  sassOptions: {
-      additionalData: `
+    reactStrictMode: false,
+    sassOptions: {
+        additionalData: `
         @import "app/scss/_vars.scss";
         @import "app/scss/_mixins.scss";
       `,
-  },
-  experimental: { esmExternals: true },
-  images: {
-      domains: ['*'],
-  },
-  outputFileTracing: true
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    experimental: { esmExternals: true },
+    images: {
+        domains: ["*"],
+    },
+    outputFileTracing: true,
 };
 
 export default nextConfig;
