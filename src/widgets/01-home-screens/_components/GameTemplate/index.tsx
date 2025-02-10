abf4fb316d535607591421122961862aeca58c04
NotDaymere <ikonotopchick@gmail.com>
2025-02-10 16:16:45 +0200

    add micro animation (home)

--- src/widgets/01-home-screens/_components/GameTemplate/index.tsx
+++ src/widgets/01-home-screens/_components/GameTemplate/index.tsx
@@ -9,27 +9,23 @@ interface Props {
     icons?: 1 | 2 | 3;
 }
 
-export const GameTemplate: React.FC<Props> = ({
-    title,
-    children,
-    icons
-}) => {
+export const GameTemplate: React.FC<Props> = ({ title, children, icons }) => {
     return (
         <div className={css.game}>
             <div className="container">
                 <div className={css.game_wrapper}>
-                    <Image.Default 
+                    <Image.Default
                         className={css.game_background}
                         src="/img/home/game-bg.webp"
                         alt=""
                     />
                     <div className={css.game_title_container}>
-                        <Image.Default 
+                        <Image.Default
                             className={css.game_title_stars_1}
                             src="/img/home/game-stars.png"
                             alt=""
                         />
-                        <Image.Default 
+                        <Image.Default
                             className={css.game_title_stars_2}
                             src="/img/home/game-stars.png"
                             alt=""
@@ -38,51 +34,37 @@ export const GameTemplate: React.FC<Props> = ({
                             <Title className={css.game_title} text={title} animated />
                             {icons === 1 && (
                                 <>
-                                    <Image.Default 
-                                        className={css.game_box_clubs}
-                                        src="/img/figures/clubs.svg"
-                                        alt=""
-                                    />
-                                    <Image.Default 
-                                        className={css.game_box_hearts}
-                                        src="/img/figures/hearts.svg"
-                                        alt=""
-                                    />
+                                    <div className={css.game_box_clubs}>
+                                        <Image.Default src="/img/figures/clubs.svg" alt="" />
+                                    </div>
+                                    <div className={css.game_box_hearts}>
+                                        <Image.Default src="/img/figures/hearts.svg" alt="" />
+                                    </div>
                                 </>
                             )}
                             {icons === 2 && (
                                 <>
-                                    <Image.Default 
-                                        className={css.game_box_blueChip}
-                                        src="/img/figures/blue-chip.png"
-                                        alt=""
-                                    />
-                                    <Image.Default 
-                                        className={css.game_box_greenChip}
-                                        src="/img/figures/green-chip.png"
-                                        alt=""
-                                    />
+                                    <div className={css.game_box_blueChip}>
+                                        <Image.Default src="/img/figures/blue-chip.png" alt="" />
+                                    </div>
+                                    <div className={css.game_box_greenChip}>
+                                        <Image.Default src="/img/figures/green-chip.png" alt="" />
+                                    </div>
                                 </>
                             )}
                             {icons === 3 && (
                                 <>
-                                    <Image.Default 
-                                        className={css.game_box_clubsRight}
-                                        src="/img/figures/clubs.svg"
-                                        alt=""
-                                    />
-                                    <Image.Default 
-                                        className={css.game_box_square}
-                                        src="/img/figures/square.svg"
-                                        alt=""
-                                    />
+                                    <div className={css.game_box_clubsRight}>
+                                        <Image.Default src="/img/figures/clubs.svg" alt="" />
+                                    </div>
+                                    <div className={css.game_box_square}>
+                                        <Image.Default src="/img/figures/square.svg" alt="" />
+                                    </div>
                                 </>
                             )}
                         </div>
                     </div>
-                    <div className={`${css.game_content} game-template-content`}>
-                        {children}
-                    </div>
+                    <div className={`${css.game_content} game-template-content`}>{children}</div>
                 </div>
             </div>
         </div>
